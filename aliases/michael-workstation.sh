aa8ff868fdca43d675f6a7bfb262b69ae1c13e98
Michael <mmaccoffe@gmail.com>
2013-02-12 20:55:24 +0200

    Changes

--- aliases/michael-workstation.sh
+++ aliases/michael-workstation.sh
@@ -1,19 +1,10 @@
-alias rkt='rake --rakefile ./rake-tasts/Rakefile'
-
-alias rs='./script/server -e development'
-alias rsu='./script/server -e development -u'
-alias rc='./script/console development'
-alias rkr='rake routes > ../rake-routes.tmp; sublime-text ../rake-routes.tmp'
-alias s='sublime-text'
-alias rscript='ruby ./tmp/rails-script.rb'
-
-alias docs='rvmsudo ruby ~/ruby-projects/webrick/server.rb'
+alias      rs='./script/server -e development'
+alias      rc='./script/console development'
 
 alias down='cd ~/Downloads'
-alias rp='cd ~/rails-projects'
-alias rb='cd ~/ruby-projects'
-alias ae='cd ~/rails-projects/artistecard'
-alias sb='cd ~/rails-projects/sandbox'
+alias   rp='cd ~/rails-projects'
+alias   rb='cd ~/ruby-projects'
+alias   ae='cd ~/rails-projects/artistecard'
 
 alias vimdir='cd ~/.vim'
-alias ch='vim ~/Dropbox/linux_cheatsheet.txt'
+alias     ch='vim ~/Dropbox/linux_cheatsheet.txt'
