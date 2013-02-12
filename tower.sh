cac128f442f6c38e217e95f50c3917dcde141a58
Michael <mmaccoffe@gmail.com>
2013-02-12 09:53:01 +0200

    Aliasing

--- tower.sh
+++ tower.sh
@@ -7,29 +7,12 @@ config_file=config.sh
 
 #Initialization
 ####################################
-# Getting directory with this script
-# __tower_root_dir() {
-#   (cd "$( dirname "${BASH_SOURCE}[0]" )" && pwd)
-# }
-# __root_dir=
-# __get_aliases_from () {
-#   local alias_dir=aliases
-#   local root=$1
-#   local cur_hostname=$(hostname).sh
-#   
-#   local t=$root/$alias_dir/$cur_hostname
-#   [[ -s $t ]] && source $t
-# }
-# 
-# __get_aliases_from $(__tower_root_dir)
-# 
-# source aliases.sh
 
 __get_file_by_hostname_from() {
   local dir=$1
   local root=$(cd "$( dirname "${BASH_SOURCE}[0]" )" && pwd)
   file=$(hostname).sh
-  "$root/$dir/$file"
+  echo $root/$dir/$file
 }
 
 __source_silently() {
@@ -37,3 +20,7 @@ __source_silently() {
 }
 
 __source_silently $( __get_file_by_hostname_from aliases )
+__source_silently aliases.sh
+
+alias gl_a='\vim aliases.sh'
+alias l_a="\vim $(__get_file_by_hostname_from aliases)"
